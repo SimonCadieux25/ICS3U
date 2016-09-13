7bd8d637c102ba16a0c43376f3a336a8d7a54a58
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2016-09-13 13:34:35 -0400

    Sep 13, 2016 Program

---
+++ Hello/src/cadieux/aboutMe.java
@@ -0,0 +1,14 @@
+package cadieux;
+
+public class aboutMe {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Simon C.");
+		System.out.println("Kemp");
+		System.out.println("Runnymede C.I.");
+		System.out.println("Go Ravens Go");
+
+	}
+
+}
