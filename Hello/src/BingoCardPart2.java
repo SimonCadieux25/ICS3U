44918fb632e1b27e73847fde905c878f096f22f6
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2016-09-16 12:26:42 -0400

    Pingo Card part 2

---
+++ Hello/src/BingoCardPart2.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ *
+ */
+public class BingoCardPart2 {
+
+	public static void main(String[] args) {
+		System.out.println("Bingo Card\n");
+		System.out.println("1. The Caller randomly pulls a nummbered bingo ball.\n");
+		System.out.println("2. The number is placed on the bingo board and called out.\n");
+		System.out.println("3. Players look for the called number on their bingo card.\n");
+		System.out.println("4. If the number is located, it is marked off.\n");
+		System.out.println("5. Steps 1 to 4 are repeated untill a player maches the BINGO patern.\n");
+		System.out.println("6. The winning player yells BINGO.");
+
+	}
+
+}
