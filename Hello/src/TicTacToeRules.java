a9aca8ec74fad9397d3b3c04223ee608b8b1a6b8
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2016-09-19 12:54:32 -0400

    Pushing TicTacToeRules

---
+++ Hello/src/TicTacToeRules.java
@@ -0,0 +1,21 @@
+/* TicTacToeRules
+ * States the rules of Tic-Tac-Toe 
+ * SimonCadieux
+ * Sept 19, 2016
+ */
+public class TicTacToeRules {
+
+
+	public static void main(String[] args) {
+		System.out.println("Tic Tac Toe Rules\n");
+		System.out.println("Tic Tac Toe is a game for 2 players.\n");
+		System.out.println("1. Agrid of 9 squares is drawn.\n");
+		System.out.println("2. The firs player draws an X in an empty square.\n");
+		System.out.println("3. The second player draws an O in an empty square.\n");
+		System.out.println("4. Steps 2 and 3 are repeated until a Tic Tac patern is created:\n");
+		System.out.println("   Patterns can be diagonal, horisontal, or vertical.\n");
+		System.out.println("5. A winning pattern is completed by drawing a line through the pattern.\n");
+
+	}
+
+}
