ed4fb5671f4ae11dc31487e650022dcc967e7261
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2016-09-19 12:41:54 -0400

    Pushing Tic-Tac-Toe Board

---
+++ Hello/src/TicTacToeBoard.java
@@ -0,0 +1,23 @@
+/* TicTacToeBoard
+ * Generates a Tic-Tac-Toe Board
+ * SimonCadieux
+ * Sept 19, 2016
+ */
+public class TicTacToeBoard {
+
+	public static void main(String[] args) {
+		System.out.println("\t|\t|");
+		System.out.println("\t|\t|");
+		System.out.println("\t|\t|");
+		System.out.println("  ---------------------");
+		System.out.println("\t|\t|");
+		System.out.println("\t|   X\t|");
+		System.out.println("\t|\t|");
+		System.out.println("  ---------------------");
+		System.out.println("\t|\t|");
+		System.out.println("\t|\t|");
+		System.out.println("\t|\t|");
+
+	}
+
+}
