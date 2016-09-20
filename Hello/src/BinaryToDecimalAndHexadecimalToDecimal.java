21dee3c1688cdbed3b0f92036c35cc080b10b7fd
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2016-09-20 13:03:26 -0400

    Repushing the updated Decimal convertion

--- Hello/src/BinaryToDecimalAndHexadecimalToDecimal.java
+++ Hello/src/BinaryToDecimalAndHexadecimalToDecimal.java
@@ -4,45 +4,31 @@
  * Sept 19, 2016
  */
 public class BinaryToDecimalAndHexadecimalToDecimal {
-	public static void main(String[] args) {
-		System.out.format("%-7s%7s%-7s","Decimal","","Binary\n");
-		System.out.format("%7s%7s%7s","1","","0\n");
-		System.out.format("%7s%7s%7s","2","","1\n");
-		System.out.format("%7s%7s%7s","3","","10\n");
-		System.out.format("%7s%7s%7s","4","","11\n");
-		System.out.format("%7s%7s%7s","5","","100\n");
-		System.out.format("%7s%7s%7s","6","","101\n");
-		System.out.format("%7s%7s%7s","7","","110\n");
-		System.out.format("%7s%7s%7s","8","","111\n");
-		System.out.format("%7s%7s%7s","9","","1000\n");
-		System.out.format("%7s%7s%7s","10","","1001\n");
-		System.out.format("%7s%7s%7s","11","","1010\n");
-		System.out.format("%7s%7s%7s","12","","1100\n");
-		System.out.format("%7s%7s%7s","13","","1101\n");
-		System.out.format("%7s%7s%7s","14","","1110\n");
-		System.out.format("%7s%7s%7s","15","","1111\n");
-		System.out.format("%7s%7s%7s","16","","10000\n");
-		System.out.format("%7s%7s%7s","17","","10001\n");
-		System.out.format("%7s%7s%7s","18","","10010\n");
-		System.out.format("%7s%7s%7s","19","","10011\n");
-		System.out.format("%7s%7s%7s","20",""," 10100\n\n");
-		System.out.format("%-7s%7s%-11s","Decimal","","Hexadecimal\n");
-		System.out.format("%7s%7s%11s","1","","0\n");
-		System.out.format("%7s%7s%11s","2","","1\n");
-		System.out.format("%7s%7s%11s","3","","2\n");
-		System.out.format("%7s%7s%11s","4","","3\n");
-		System.out.format("%7s%7s%11s","5","","4\n");
-		System.out.format("%7s%7s%11s","6","","5\n");
-		System.out.format("%7s%7s%11s","7","","6\n");
-		System.out.format("%7s%7s%11s","8","","7\n");
-		System.out.format("%7s%7s%11s","9","","8\n");
-		System.out.format("%7s%7s%11s","10","","9\n");
-		System.out.format("%7s%7s%11s","11","","A\n");
-		System.out.format("%7s%7s%11s","12","","B\n");
-		System.out.format("%7s%7s%11s","13","","C\n");
-		System.out.format("%7s%7s%11s","14","","D\n");
-		System.out.format("%7s%7s%11s","15","","E\n");
-		System.out.format("%7s%7s%11s","16","","F\n");
+	
+	public static void main(String[] args){
+		System.out.format("%-7s%7s%-7s%7s%-11s","Decimal","","Binary","","Hexadecimal\n");
+		System.out.format("%7s%7s%7s%7s%11s","1","","0","","1\n");
+		System.out.format("%7s%7s%7s%7s%11s","2","","1","","2\n");
+		System.out.format("%7s%7s%7s%7s%11s","3","","10","","3\n");
+		System.out.format("%7s%7s%7s%7s%11s","4","","11","","4\n");
+		System.out.format("%7s%7s%7s%7s%11s","5","","100","","5\n");
+		System.out.format("%7s%7s%7s%7s%11s","6","","101","","6\n");
+		System.out.format("%7s%7s%7s%7s%11s","7","","110","","7\n");
+		System.out.format("%7s%7s%7s%7s%11s","8","","111","","8\n");
+		System.out.format("%7s%7s%7s%7s%11s","9","","1000","","9\n");
+		System.out.format("%7s%7s%7s%7s%11s","10","","1001","","A\n");
+		System.out.format("%7s%7s%7s%7s%11s","11","","1010","","B\n");
+		System.out.format("%7s%7s%7s%7s%11s","12","","1011","","C\n");
+		System.out.format("%7s%7s%7s%7s%11s","13","","1100","","D\n");
+		System.out.format("%7s%7s%7s%7s%11s","14","","1101","","E\n");
+		System.out.format("%7s%7s%7s%7s%11s","15","","1110","","F\n");
+		System.out.format("%7s%7s%7s%7s%11s","16","","1111","","10\n");
+		System.out.format("%7s%7s%7s%7s%11s","17","","10000","","11\n");
+		System.out.format("%7s%7s%7s%7s%11s","18","","10001","","12\n");
+		System.out.format("%7s%7s%7s%7s%11s","19","","10010","","13\n");
+		System.out.format("%7s%7s%7s%7s%11s","20","","10011","","14\n");
 	}
+
+
 
 }
