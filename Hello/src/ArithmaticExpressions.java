088d23b2aa5ad9847889fd451a0dbbe32749bc8e
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2016-10-11 13:12:59 -0400

    Commiting fast food restaurant program

--- Hello/src/ArithmaticExpressions.java
+++ Hello/src/ArithmaticExpressions.java
@@ -24,6 +24,7 @@ public class ArithmaticExpressions {
 		num3 = scan.nextInt();
 		num4 = scan.nextInt();
 		num5 = scan.nextInt();
+		
 		System.out.println("The sum of all 5 numbers: "+ (num1+num2+num3+num4+num5));
 		System.out.println("Second number - Third number: "+ (num2-num3));
 		System.out.println("First number X the fifth number: "+ (num1*num5));
@@ -31,6 +32,7 @@ public class ArithmaticExpressions {
 		System.out.println("remainder when dividing the fourth number by the second: "+ (num4%num2)); 	
 		System.out.println("first number raised to the power of the third number: "+ (num1^num3));
 		System.out.println("square root of the fifth number: "+ Math.sqrt(num5));
+		
 	}
 
 }
---
+++ Hello/src/chapter2/FastFoodRestaurant.java
@@ -0,0 +1,54 @@
+package chapter2;
+
+/*
+ * FastFoodRestaurant
+ * Crates a recipt for a fast food restaurant
+ * Simon Cadieux
+ * Oct 6 2016
+ */
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class FastFoodRestaurant {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		 DecimalFormat df = new DecimalFormat ("0.00");
+		
+		int numofb;
+		int numoff;
+		int numofs;
+		double tax = 0.065;
+		double amountpaid;
+		
+		double priceofb = 1.69;
+		double priceoff = 1.09;
+		double priceofs = 0.99;
+		
+		double beforetax;
+		double total;
+		double taxcal;
+		double change;
+		
+		System.out.print("Enter the number of burgers: "); numofb = scan.nextInt();
+		System.out.print("Enter the number of fries: "); numoff = scan.nextInt();
+		System.out.print("Enter the number of sodas: "); numofs = scan.nextInt();
+		beforetax = ((priceofb*numofb)+(priceoff*numoff)+(priceofs*numofs));
+		System.out.println("Total Before Tax: $"+ df.format(beforetax));
+		taxcal = (beforetax*tax);
+		System.out.println("Tax: $"+ df.format(taxcal));
+		total = (taxcal+beforetax);
+		System.out.println("Total: $"+ df.format(total));
+		
+		System.out.print("\n Enter amount tendered: $"); amountpaid = scan.nextInt();
+		change = (amountpaid-total);
+		System.out.println("Change: $"+df.format(change));
+		
+		
+
+	}
+
+}
