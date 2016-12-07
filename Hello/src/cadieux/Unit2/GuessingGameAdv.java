063300d76d56972441d201ad577f8aa52a9fe715
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2016-12-07 13:33:00 -0500

    ssssssssssssssssssssssssssssss

--- Hello/src/cadieux/Unit2/GuessingGameAdv.java
+++ Hello/src/cadieux/Unit2/GuessingGameAdv.java
@@ -16,12 +16,13 @@ public class GuessingGameAdv {
 		
 		System.out.print("Enter a number between 1 and 20: ");
 		int num = scan.nextInt();
+		int guess = 0;
 		
 		int[] guesses = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 		
 		for(int i = 1; i<=20; i++){
 			System.out.print("Computer guesses: ");
-			int guess = (int)(Math.random()*19);
+			guess = (int)(Math.random()*19);
 			System.out.println(guesses[guess]);
 			
 			if(guesses[guess]!=num){
@@ -35,8 +36,11 @@ public class GuessingGameAdv {
 		if(num>20||num<1){
 			System.out.println("You cheated: the number entered is not between 1 and 20");
 		}
-		else{
+		else if(guesses[guess]!=num){
 			System.out.println("You Win the computer could not guess your number");
+		}
+		else{
+			
 		}
 		
 	}
