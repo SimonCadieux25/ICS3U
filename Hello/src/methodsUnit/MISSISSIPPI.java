b7281d3722dd730f043c02f87e57bf08b71d8736
333716231 <333716231@C-2DF4TJ1.tdsb.on.ca>
2017-01-11 13:28:24 -0500

    Commiting MISSISSIPPI program

---
+++ Hello/src/methodsUnit/MISSISSIPPI.java
@@ -0,0 +1,70 @@
+package methodsUnit;
+/**
+ * MISSISSIPPI.java
+ * prints out MISSISSIPPI using methods
+ * Simon Cadieux
+ * Jan 11 2017
+ */
+public class MISSISSIPPI {
+
+	public static void printM(){
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+	}
+	public static void printI(){
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+	}
+	public static void printS(){
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S     ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+	}
+	public static void printP(){
+		System.out.println("PPPPP ");
+		System.out.println("P    P");
+		System.out.println("P    P");
+		System.out.println("PPPPP ");
+		System.out.println("P    ");
+		System.out.println("P    ");
+		System.out.println("P    ");
+		System.out.println("P    ");
+	}
+	public static void main(String[] args) {
+		printM();
+		System.out.println();
+		printI();
+		System.out.println();
+		printS();
+		System.out.println();
+		printS();
+		System.out.println();
+		printI();
+		System.out.println();
+		printS();
+		System.out.println();
+		printS();
+		System.out.println();
+		printI();
+		System.out.println();
+		printP();
+		System.out.println();
+		printP();
+		System.out.println();
+		printI();
+	}
+
+}
